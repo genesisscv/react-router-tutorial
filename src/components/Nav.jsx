ae2c15a16370d5f6c75c54eca9a3931b85c23510
genesisscv <genesisscv@gmail.com>
2022-08-02 20:28:32 +1000

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
         <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/">Home </Link>
+        <Link to="/about">About </Link>
+        <Link to="/contact">Contact </Link>
         </nav> 
     );
 }
